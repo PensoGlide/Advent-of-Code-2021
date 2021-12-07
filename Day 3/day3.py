0b6763b442535cb76ef29decd18bb490a5ac141e
PensoGlide <eduardo.augusto.santiago@gmail.com>
2021-12-07 00:48:25 +0000

    Added shit program

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def getData(filePath):
+def getData(filePath, inInt=False):
 
     raw_data = open(filePath, 'r')
     data = raw_data.read()
     data = data.split('\n')
+
+    if inInt == True:
+        data = [int(nr) for nr in data]
     
     return data
 
@@ -27,7 +30,7 @@ def getCommon_Uncommon(data):
 
     for element in result[0]:
         print(element)
-        if element > 0.5:
+        if element >= 0.5:
             common.append(1)
             uncommon.append(0)
         else:
@@ -39,10 +42,10 @@ def getCommon_Uncommon(data):
 
     return common, uncommon
 
-gamma, epsilon = getCommon_Uncommon(data)
+common, uncommon = getCommon_Uncommon(data)
 
-gamma   = int(gamma,2)
-epsilon = int(epsilon,2)
+gamma   = int(common,2)
+epsilon = int(uncommon,2)
 
 
 print(gamma*epsilon)
@@ -50,3 +53,85 @@ print(gamma*epsilon)
 
 ##############################################
 # Part 2
+
+
+def oxygen_generator_rating(data):
+
+    common, uncommon = getCommon_Uncommon(data)
+    common_bit = int(common[0])
+    first_bits = np.array([ int(val[0]) for val in data])
+    mask = np.ma.masked_equal(first_bits, common_bit).mask
+    data = np.array(data)
+    numbers = data[mask]
+
+
+    for i in range(1,len(data[0])):
+        print(numbers)
+
+        if len(numbers) == 1:
+            return numbers
+
+        else:
+            common, uncommon = getCommon_Uncommon(numbers)
+            print(common, i)
+            if common[i] == 3:
+                common_bit = 1
+            else:
+                common_bit = int(common[i])
+            first_bits = [ int(val[i]) for val in numbers]
+
+            mask = np.ma.masked_equal(first_bits, common_bit).mask
+
+            print(first_bits, common_bit, mask)
+        
+        numbers = np.array(numbers)
+        numbers = numbers[mask]
+
+    return numbers
+
+
+
+def CO2_rating(data):
+
+    common, uncommon = getCommon_Uncommon(data)
+    common_bit = int(uncommon[0])
+    first_bits = np.array([ int(val[0]) for val in data])
+    mask = np.ma.masked_equal(first_bits, common_bit).mask
+    data = np.array(data)
+    numbers = data[mask]
+
+
+    for i in range(1,len(data[0])):
+        print(numbers)
+
+        if len(numbers) == 1:
+            return numbers
+
+        else:
+            common, uncommon = getCommon_Uncommon(numbers)
+            print(common, i)
+            if uncommon[i] == 3:
+                common_bit = 1
+            else:
+                common_bit = int(uncommon[i])
+            first_bits = [ int(val[i]) for val in numbers]
+
+            mask = np.ma.masked_equal(first_bits, common_bit).mask
+
+            print(first_bits, common_bit, mask)
+        
+        numbers = np.array(numbers)
+        numbers = numbers[mask]
+
+    return numbers
+
+
+ox  = oxygen_generator_rating(data)
+co2 = CO2_rating(data)
+
+ox   = int(ox[0],2)
+co2  = int(co2[0],2)
+
+print(ox, co2)
+
+print(ox*co2)
